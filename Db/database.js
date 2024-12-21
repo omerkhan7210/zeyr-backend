5bbf0613c416da0243cd6c9e2ee99ce4cc094b1a
omer khan <omerfarooqkhan7210@gmail.com>
2024-12-21 12:24:48 +0500

    db

--- Db/database.js
+++ Db/database.js
@@ -1,15 +1,14 @@
-import mysql from 'mysql';
+import mysql from 'mysql2';
 
 const pool = mysql.createConnection({
   host: '185.97.146.100', // Host should only contain the IP or domain
   port: 3306,             // Add port number separately
   user: 'root',           // Your MySQL username
   password: 'Umerkhan@1122@', // Your MySQL password
-  database: 'zeyr-custom',    // Your database name
+  database: 'zeyrcustom',    // Your database name
 });
 
-// db
-
+// db connection
 pool.connect((err) => {
   if (err) {
     console.error('Error connecting to the database:', err);
